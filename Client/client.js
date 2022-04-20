adae58e978535b8c56961c8b9b929710f6d3249d
ManayLaptop <manay.patel.1@gmail.com>
2022-04-19 20:55:27 -0400

    created file structures for easier working

---
+++ Client/client.js
@@ -0,0 +1,37 @@
+//All API calls will be made here.
+//Example:
+// export async function readCounter(name) {
+//   try {
+//     const response = await fetch(`/read?name=${name}`, {
+//       method: "GET",
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+export async function courseRatings(courseName, UniName) {
+  try {
+    const response = await fetch(`/courseRatings?course=${courseName}&uniName=${UniName}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function uniCourses(UniName) {
+    try {
+      const response = await fetch(`/uniCourses?uniName=${UniName}`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
